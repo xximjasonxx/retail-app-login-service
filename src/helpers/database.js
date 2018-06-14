1a6977865b2c808268a84e5638d6a9add2cedc56
Jason Farrell <imjason@gmail.com>
2018-06-13 21:04:33 -0500

    Reformatting connection string determination

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,12 +1,6 @@
 
 const pgp = require('pg-promise')();
+console.log(`connStr: postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/Users`);
 
-const buildPgConnectionString = () => {
-    return `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/Users`;
-};
-
-const connectionString = buildPgConnectionString();
-console.log(`connStr: ${connectionString}`);
-
-const db = pgp(connectionString);
+const db = pgp(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/Users`);
 exports.database = db;
