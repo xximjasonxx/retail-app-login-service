bbe1cdfd597db0df10a220f15e9708f0a95c18cf
Jason Farrell <imjason@gmail.com>
2018-06-13 20:54:16 -0500

    Updates to connection string determination

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,12 +1,12 @@
 
 const pgp = require('pg-promise')();
 
+const buildPgConnectionString = () => {
+    return `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/Users`;
+};
+
 const connectionString = buildPgConnectionString();
 console.log(connectionString);
 
 const db = pgp(connectionString);
 exports.database = db;
-
-const buildPgConnectionString = () => {
-    return `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/Users`;
-};
