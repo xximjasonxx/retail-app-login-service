d884c2be36e40599c10f8eff951000470ef63bad
Jason Farrell <imjason@gmail.com>
2018-06-13 21:08:58 -0500

    Trying to determine if process is undefined

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,6 +1,6 @@
 
 const pgp = require('pg-promise')();
-console.log(`connStr: postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/Users`);
+console.log(JSON.stringify(process));
 
 const db = pgp(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/Users`);
 exports.database = db;
